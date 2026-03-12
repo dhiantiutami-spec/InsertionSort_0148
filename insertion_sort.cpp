e4bec366b6ebc29a9af6e6831451c5a5f24123a8
dhiantiutami-spec <dhiantiutami@gmail.com>
2026-03-12 11:10:49 +0700

    membuat fungsi display

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -54,3 +54,16 @@ void insertionsort()
         }
     }
 }
+
+void display(){
+    cout << endl;
+    cout << "\n=================";
+    cout << "\nElement Array";
+    cout << "\n=================";
+
+    for(int j = 0; j < n; j++){
+        cout << arr[j] << endl;
+    }
+
+    cout << endl;
+}
